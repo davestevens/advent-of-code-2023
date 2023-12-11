886005515b15ab3ceeae842099dafae3e4342945
Dave Stevens <hello@davestevens.co.uk>
2023-12-11 07:48:50 +0000

    feat: day 11

---
+++ 11/part2.js
@@ -0,0 +1,62 @@
+const input = `...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....`;
+
+const grid = input.split("\n").map(line => {
+  return line.split('');
+});
+
+const GALAXY = '#';
+const MULTIPLIER = 1000000;
+
+// find empty rows/columns
+const emptyRows = [];
+for (let i = 0; i < grid.length; ++i) {
+  const row = grid[i];
+  if (row.every(cell => cell != GALAXY)) {
+    emptyRows.push(i);
+  }
+}
+
+const emptyCols = [];
+for (let i = 0; i < grid[0].length; ++i) {
+  const col = grid.map(row => row[i]);
+  if (col.every(cell => cell != GALAXY)) {
+    emptyCols.push(i);
+  }
+}
+
+// find all galaxies
+const galaxies = [];
+grid.forEach((row, rowIndex) => {
+  row.forEach((cell, colIndex) => {
+    if (cell === GALAXY) {
+      galaxies.push({ rowIndex, colIndex });
+    }
+  });
+});
+
+const calculateDistance = (from, to) => {
+  const [minRow, maxRow] = [from.rowIndex, to.rowIndex].sort((a, b) => a - b);
+  const [minCol, maxCol] = [from.colIndex, to.colIndex].sort((a, b) => a - b);
+  const distance = (maxRow - minRow) + (maxCol - minCol);
+  const emptyRowCount = emptyRows.filter(emptyRow => emptyRow > minRow && emptyRow < maxRow).length;
+  const emptyColCount = emptyCols.filter(emptyCol => emptyCol > minCol && emptyCol < maxCol).length;
+  return distance + ((emptyRowCount + emptyColCount) * (MULTIPLIER - 1));
+};
+
+// calculate all distance pairs
+let distance = 0;
+for (let i = 0; i < galaxies.length; ++i) {
+  for (let j = i + 1; j < galaxies.length; ++j) {
+    distance += calculateDistance(galaxies[i], galaxies[j]);
+  }
+}
+console.log(distance);
